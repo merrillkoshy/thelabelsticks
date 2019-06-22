51890428fe6b10dd39583cdebff0a307903b749c
merrillkoshy <merrillkoshy@gmail.com>
2019-06-22 16:42:26 +0400

    initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showPopup: false
+    };
+  }
+  render() {
+    return <div>Hello Highland</div>;
+  }
+}
+export default Home;
