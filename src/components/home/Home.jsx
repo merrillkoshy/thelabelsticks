ce3b77854b023a16d324a29e1b7f65bfb1216223
merrillkoshy <merrillkoshy@gmail.com>
2019-06-23 22:14:40 +0400

    Flipbook

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./Home.css";
+import FlipBook from "../flipbook/FlipBook";
 
 class Home extends Component {
   constructor(props) {
@@ -7,7 +8,8 @@ class Home extends Component {
     this.handleMouseHover = this.handleMouseHover.bind(this);
     this.state = {
       isHovering: false,
-      isClickedReadMore:false
+      isClickedReadMore: false,
+      date: new Date()
     };
   }
   componentDidMount() {
@@ -18,11 +20,29 @@ class Home extends Component {
   }
   toggleHoverState(state) {
     return {
-      isHovering: !state.isHovering,
+      isHovering: !state.isHovering
     };
-  } 
-  
+  }
+
   render() {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    const month = () => {
+      return months[this.state.date.getMonth()];
+    };
+    const showFlipBook = item => this.setState({ showFlipBook: true });
     return (
       <div className="home">
         <div className="masthead">
@@ -31,31 +51,54 @@ class Home extends Component {
             src="https://thelabelsticks.com/tls_red.png"
             alt="The Label Sticks"
           />
+          <div className="timestamp">
+            {this.state.date.getDate()} {month()}{" "}
+            {this.state.date.getFullYear()}
+          </div>
         </div>
         <div className="menu">
-          <a className="menuitem" onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseHover}>
-          
+          <a
+            className="menuitem"
+            onMouseEnter={this.handleMouseHover}
+            onMouseLeave={this.handleMouseHover}
+          >
             OUR STARS
-            {this.state.isHovering && <div className="hover">
-              <h1>OUR STARS</h1>
-              <div className="imagewrapper">                
-                <img src="https://thelabelsticks.com/assets/homepage-section/our-stars.jpg"/>                
-              <p>The Label Sticks features celebrities that have outshone the crowd and created labels that glam the world. These stars form our galaxy called fashion.</p>
-              <button >READ</button>
+            {this.state.isHovering && (
+              <div className="hover">
+                <h1>OUR STARS</h1>
+                <div className="imagewrapper">
+                  <img src="https://thelabelsticks.com/assets/homepage-section/our-stars.jpg" />
+                  <p>
+                    The Label Sticks features celebrities that have outshone the
+                    crowd and created labels that glam the world. These stars
+                    form our galaxy called fashion.
+                  </p>
+                  <button onClick={() => this.showFlipBook("stars")}>
+                    READ
+                  </button>
+                </div>
               </div>
-              
-              </div>}
+            )}
             <p>
               Celebrities that have outshone <strong>the crowd</strong>
             </p>
           </a>
-          <a className="menuitem" style={{fontFamily: 'Rubik Mono One, sans-serif',fontWeight: '500'}}>
+          <a
+            className="menuitem"
+            style={{
+              fontFamily: "Rubik Mono One, sans-serif",
+              fontWeight: "500"
+            }}
+          >
             ROYALTY
             <p>Here’s a window to their life</p>
           </a>
-          <a className="menuitem" style={{fontFamily: 'Abril Fatface, cursive',fontWeight: '500'}}>
+          <a
+            className="menuitem"
+            style={{ fontFamily: "Abril Fatface, cursive", fontWeight: "500" }}
+          >
             VANITY BOOST
-            <p style={{fontFamily: 'serif'}}>
+            <p style={{ fontFamily: "serif" }}>
               Confidence comes from within you but what’s outside matters just
               as much
             </p>
@@ -64,23 +107,54 @@ class Home extends Component {
             BOUTIQUE BRANDS
             <p>Nothing haunts us like the things we didn't buy</p>
           </a>
-          <a className="menuitem" style={{fontFamily: 'Rubik Mono One, sans-serif',fontWeight: '500',fontStyle: 'italic',    letterSpacing: '-0.3vmin'}}>
+          <a
+            className="menuitem"
+            style={{
+              fontFamily: "Rubik Mono One, sans-serif",
+              fontWeight: "500",
+              fontStyle: "italic",
+              letterSpacing: "-0.5vmin",
+              fontSize: "calc(2vmin + 3px)"
+            }}
+          >
             LEARNING CURVE
-            <p style={{letterSpacing: '0vmin',fontStyle: 'normal'}}>Know the stars that shine through their students</p>
+            <p style={{ letterSpacing: "0vmin", fontStyle: "normal" }}>
+              Know the stars that shine through their students
+            </p>
           </a>
-          <a className="menuitem" style={{position: 'absolute',right: '1rem',top: '60%'}}>
+          <a
+            className="menuitem"
+            style={{ position: "absolute", right: "1rem", top: "60%" }}
+          >
             BON VOYAGE
-            <p style={{fontFamily: 'serif'}}> We’re here to guide you through</p>
+            <p style={{ fontFamily: "serif" }}>
+              {" "}
+              We’re here to guide you through
+            </p>
           </a>
-          <a className="menuitem" style={{fontFamily: 'Abril Fatface, cursive',fontWeight: '500',position: 'absolute',right: '2.5rem',top: '78%'}}>
+          <a
+            className="menuitem"
+            style={{
+              fontFamily: "Abril Fatface, cursive",
+              fontWeight: "500",
+              position: "absolute",
+              right: "2.5rem",
+              top: "78%"
+            }}
+          >
             DESTINATIONS
-            <p> Trending <strong>properties</strong> and property related news in a nutshell, right here</p>
+            <p>
+              {" "}
+              Trending <strong>properties</strong> and property related news in
+              a nutshell, right here
+            </p>
           </a>
         </div>
         <img
           alt="The Labelsticks director Ms.Sandy Nathan"
           src="https://thelabelsticks.com/assets/tls_cover.jpg"
         />
+        {this.state.showFlipBook && <FlipBook />}
       </div>
     );
   }
