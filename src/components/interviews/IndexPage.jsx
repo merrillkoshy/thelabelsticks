f75b4ae81264fb19d1a4693f5105e22e4e1b7c0d
merrillkoshy <merrillkoshy@gmail.com>
2019-07-02 22:08:48 +0400

    cue card

--- src/components/interviews/IndexPage.jsx
+++ src/components/interviews/IndexPage.jsx
@@ -5,9 +5,10 @@ class IndexPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      toggled:false,
-      clickedCard:'',
-      readMore:''
+      toggled: false,
+      clickedCard: "",
+      readMore: "",
+      revertCard: ""
     };
     this.toggleMenu = this.toggleMenu.bind(this);
   }
@@ -22,17 +23,16 @@ class IndexPage extends Component {
 
   toggleMenu() {
     let isToggled = this.state.toggled;
-    this.setState({ toggled: !isToggled});
+    this.setState({ toggled: !isToggled });
   }
   componentDidUpdate() {
     console.log("componentDidUpdate fired");
     console.log("STATE", this.state);
   }
-  backButton=()=>{
-    this.setState({ clickedCard:""});
+  backButton = () => {
+    this.setState({ clickedCard: "" });
     console.log(this.state.clickedCard);
-  }
-  
+  };
 
   render() {
     const allStarsListJSON = JSON.parse(localStorage.getItem("stars"));
@@ -49,7 +49,12 @@ class IndexPage extends Component {
       <div style={{ position: "absolute" }}>
         <div className="intro-cover-black" />
         <div className="introCover">
-          <video autoPlay muted loop className={"backgroundVideo "+this.state.readMore}>
+          <video
+            autoPlay
+            muted
+            loop
+            className={"backgroundVideo " + this.state.readMore}
+          >
             <source
               src="https://thelabelsticks.com/assets/ourstars/ramp.mp4"
               type="video/mp4"
@@ -58,48 +63,117 @@ class IndexPage extends Component {
           <h1>
             {item.id} | {item.firstname + " " + item.lastname}
           </h1>
-          <div className={"content-container "+this.state.readMore} >
+          <div className={"content-container " + this.state.readMore}>
             <div
               className="introPara"
               dangerouslySetInnerHTML={{ __html: itemDetail.startingPara }}
             />
           </div>
-          <div className={"coverForInterview " +this.state.readMore} onClick={()=>this.setState({readMore:'readMore'})}>
-          <div className="flashCardLogoContainer" style={{position: 'absolute',left: 'calc(100% - 41vmin)'}}>
-            <img src="https://thelabelsticks.com/assets/favicon.png" alt="The Label Sticks"/>
+          <div
+            className={
+              "coverForInterview " +
+              this.state.readMore +
+              (this.state.revertCard ? "inside" : "")
+            }
+            onClick={() => this.setState({ readMore: "readMore" })}
+          >
+            <div
+              className="flashCardLogoContainer"
+              style={{ position: "absolute", left: "calc(100% - 55vmin)" }}
+            >
+              <img
+                src="https://thelabelsticks.com/assets/favicon.png"
+                alt="The Label Sticks"
+              />
+            </div>
+            <img
+              style={{
+                width: "100%",
+                height: "auto",
+                position: "absolute",
+                top: "12vmin"
+              }}
+              src="https://thelabelsticks.com/assets/ourstars/anita/anita-banner.jpg"
+              alt="TLS-interview File"
+            />
+            <a
+              style={{
+                position: "absolute",
+                bottom: "10vmin",
+                left: "35vmin",
+                color: "#e4c372"
+              }}
+            >
+              THE INTERVIEW
+            </a>
+            <img
+              style={{ width: "100%", height: "auto" }}
+              id="id-logo"
+              src="https://thelabelsticks.com/assets/cardBackground.png"
+              alt="TLS-interview File"
+            />
           </div>
-          <img style={{width:"100%",height:"auto",position: "absolute",top: "12vmin"}} src="https://thelabelsticks.com/assets/ourstars/anita/anita-banner.jpg" alt="TLS-interview File"/>
-            <a style={{position:"absolute",top: "50vmin",left: "25vmin",color:'#e4c372'}}>THE INTERVIEW</a>
-              <img style={{width:"100%",height:"auto"}} id="id-logo" src="https://thelabelsticks.com/assets/cardBackground.png" alt="TLS-interview File"/>
-          </div>
-          <div className={"questionnaire-container " +this.state.readMore}>
+          <div
+            className={
+              "questionnaire-container " +
+              this.state.readMore +
+              (this.state.revertCard ? "revert" : "")
+            }
+          >
             {questionArray.map((answer, i) => {
               return (
-                
-                <div key={i+'a'}
-                onClick={this.toggleMenu} className={"flashCard " +(this.state.clickedCard===answer.number?"flipped":"")}
-                style={{left:32*i+"vmin"}}>
-                  <div onClick={() =>
-                  this.setState({ clickedCard: answer.number })}
-                  className={"flashCard-inner " +(this.state.clickedCard===answer.number?"flipped":"")}>
-                  <div className="questions">
-                  <div className="flashCardLogoContainer">
-                    <img src="https://thelabelsticks.com/assets/favicon.png" alt="The Label Sticks"/>
-                  </div>
+                <div
+                  key={i + "a"}
+                  onClick={this.toggleMenu}
+                  className={
+                    "flashCard " +
+                    (this.state.clickedCard === answer.number
+                      ? "flipped"
+                      : "") +
+                    (this.state.revertCard === answer.number ? "revert" : "")
+                  }
+                  style={{ left: 32 * i + "vmin" }}
+                >
                   <div
-                    style={{padding: '3vmin'}}
-                    key={i}
-                    dangerouslySetInnerHTML={{ __html: answer.question }}
-                  />
-                  </div>
-                  <div className="answers">
-                    <button onClick={this.backButton}>Go back</button>
-                    <div>{answer.question}</div>
-                  <div                    
-                    key={i+1}
-                    dangerouslySetInnerHTML={{ __html: answer.answer }}
-                  />
-                  </div>
+                    onClick={() =>
+                      this.setState({ clickedCard: answer.number })
+                    }
+                    className={
+                      "flashCard-inner " +
+                      (this.state.clickedCard === answer.number
+                        ? "flipped"
+                        : "") +
+                      (this.state.revertCard === answer.number ? "revert" : "")
+                    }
+                  >
+                    <div className="questions">
+                      <div className="flashCardLogoContainer">
+                        <img
+                          src="https://thelabelsticks.com/assets/favicon.png"
+                          alt="The Label Sticks"
+                        />
+                      </div>
+                      <div
+                        style={{ padding: "3vmin" }}
+                        key={i}
+                        dangerouslySetInnerHTML={{ __html: answer.question }}
+                      />
+                    </div>
+                    <div className="answers">
+                      <button
+                        onClick={() => {
+                          this.setState({ revertCard: answer.number });
+                          this.setState({ readMore: "" });
+                        }}
+                      >
+                        Go back
+                      </button>
+                      <div>{answer.question}</div>
+                      <div
+                        key={i + 1}
+                        dangerouslySetInnerHTML={{ __html: answer.answer }}
+                      />
+                    </div>
                   </div>
                 </div>
               );
